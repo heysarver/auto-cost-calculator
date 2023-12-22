001708139bfb62e3039509039bba3366186d79ec
astromodder <22250203+astromodder@users.noreply.github.com>
2023-12-22 14:03:48 -0500

    test

--- car_cost_calculator.py
+++ car_cost_calculator.py
@@ -1,15 +1,20 @@
 import requests
 
-def calculate_loan_cost(loan_amount, interest_rate, loan_length, down_payment, annual_maintenance_cost, full_insurance_cost):
-    monthly_interest_rate = interest_rate / 12 / 100
-    loan_term_in_months = loan_length * 12
-    monthly_payment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate)**loan_term_in_months) / ((1 + monthly_interest_rate)**loan_term_in_months - 1)
 
-    total_loan_cost = monthly_payment * loan_term_in_months + down_payment
-    total_maintenance_cost = annual_maintenance_cost * loan_length
-    total_insurance_cost = full_insurance_cost * loan_length
+def calculate_loan_cost(loan_amount, interest_rate, loan_length_months, down_payment, annual_maintenance_cost, full_insurance_cost):
+    monthly_interest_rate = interest_rate / 12 / 100
+    print("interest_rate: {:.2f}".format(interest_rate))
+    print("monthly_interest_rate: {:.2f}".format(monthly_interest_rate))
+    loan_term_in_years = loan_length_months / 12
+    monthly_payment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate)
+                                     ** loan_length_months) / ((1 + monthly_interest_rate)**loan_length_months - 1)
+
+    total_loan_cost = monthly_payment * loan_length_months + down_payment
+    total_maintenance_cost = annual_maintenance_cost * loan_term_in_years
+    total_insurance_cost = full_insurance_cost * loan_term_in_years
 
     return total_loan_cost + total_maintenance_cost + total_insurance_cost
+
 
 def calculate_lease_cost(lease_length, monthly_lease_payment, down_payment, annual_maintenance_cost, full_insurance_cost):
     total_lease_cost = lease_length * 12 * monthly_lease_payment + down_payment
@@ -18,20 +23,26 @@ def calculate_lease_cost(lease_length, monthly_lease_payment, down_payment, annu
 
     return total_lease_cost + total_maintenance_cost + total_insurance_cost
 
+
 def calculate_purchase_cost(purchase_price, annual_maintenance_cost, liability_insurance_cost, years_of_ownership):
     total_maintenance_cost = annual_maintenance_cost * years_of_ownership
     total_insurance_cost = liability_insurance_cost * years_of_ownership
 
     return purchase_price + total_maintenance_cost + total_insurance_cost
 
+
 def get_average_interest_rate(api_key, series_id):
-    url = f"https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json"
+    url = f"https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json&observation_start=2023-01-01"
+    print(url)
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception(f"Error fetching interest rate data: {response.status_code}")
+        raise Exception(
+            f"Error fetching interest rate data: {response.status_code}")
 
     data = response.json()
     observations = data["observations"]
-    interest_rates = [float(obs["value"]) for obs in observations if obs["value"] != "."]
-    return sum(interest_rates) / len(interest_rates)
+    interest_rates = [float(obs["value"])
+                      for obs in observations if obs["value"] != "."]
+
+    return sum(interest_rates) / len(interest_rates) / 100
--- main.py
+++ main.py
@@ -3,25 +3,22 @@ import argparse
 import os
 from car_cost_calculator import calculate_loan_cost, calculate_lease_cost, calculate_purchase_cost, get_average_interest_rate
 
-def get_argument_or_env(var_name, default_value):
-    return os.environ.get(var_name, default_value)
-
 def main():
     parser = argparse.ArgumentParser(description="Compare the total costs of leasing, getting a loan, and purchasing a car outright.")
     
-    parser.add_argument("--loan_amount", type=float, default=get_argument_or_env("LOAN_AMOUNT", 25000), help="Loan amount in USD")
-    parser.add_argument("--interest_rate", type=float, default=get_argument_or_env("INTEREST_RATE", 3.5), help="Annual interest rate for the loan as a percentage")
-    parser.add_argument("--loan_length_months", type=int, default=get_argument_or_env("LOAN_LENGTH_MONTHS", 60), help="Loan length in months")
-    parser.add_argument("--down_payment", type=float, default=get_argument_or_env("DOWN_PAYMENT", 5000), help="Down payment in USD")
-    parser.add_argument("--lease_length_months", type=int, default=get_argument_or_env("LEASE_LENGTH_MONTHS", 36), help="Lease length in months")
-    parser.add_argument("--monthly_lease_payment", type=float, default=get_argument_or_env("MONTHLY_LEASE_PAYMENT", 300), help="Monthly lease payment in USD")
-    parser.add_argument("--purchase_price", type=float, default=get_argument_or_env("PURCHASE_PRICE", 30000), help="Car purchase price in USD")
-    parser.add_argument("--annual_maintenance_cost", type=float, default=get_argument_or_env("ANNUAL_MAINTENANCE_COST", 1000), help="Annual maintenance cost in USD")
-    parser.add_argument("--full_insurance_cost", type=float, default=get_argument_or_env("FULL_INSURANCE_COST", 1500), help="Annual full insurance cost in USD")
-    parser.add_argument("--liability_insurance_cost", type=float, default=get_argument_or_env("LIABILITY_INSURANCE_COST", 800), help="Annual liability insurance cost in USD")
-    parser.add_argument("--years_of_ownership", type=int, default=get_argument_or_env("YEARS_OF_OWNERSHIP", 5), help="Number of years of ownership for the outright purchase scenario")
-    parser.add_argument("--interest_rate_series", type=str, default=get_argument_or_env("INTEREST_RATE_SERIES", "CARACBW027SBOG"), help="Interest rate series identifier")
-    parser.add_argument("--fred_api_key", type=str, default=get_argument_or_env("FRED_API_KEY", None), help="FRED API key to fetch the average interest rate")
+    parser.add_argument("--loan_amount", type=float, default=os.environ.get("LOAN_AMOUNT", 25000), help="Loan amount in USD")
+    parser.add_argument("--interest_rate", type=float, default=os.environ.get("INTEREST_RATE", 3.5), help="Annual interest rate for the loan as a percentage") # Need to fix this so it isn't overridden by FRED data
+    parser.add_argument("--loan_length_months", type=int, default=os.environ.get("LOAN_LENGTH_MONTHS", 60), help="Loan length in months")
+    parser.add_argument("--down_payment", type=float, default=os.environ.get("DOWN_PAYMENT", 5000), help="Down payment in USD")
+    parser.add_argument("--lease_length_months", type=int, default=os.environ.get("LEASE_LENGTH_MONTHS", 36), help="Lease length in months")
+    parser.add_argument("--monthly_lease_payment", type=float, default=os.environ.get("MONTHLY_LEASE_PAYMENT", 300), help="Monthly lease payment in USD")
+    parser.add_argument("--purchase_price", type=float, default=os.environ.get("PURCHASE_PRICE", 30000), help="Car purchase price in USD")
+    parser.add_argument("--annual_maintenance_cost", type=float, default=os.environ.get("ANNUAL_MAINTENANCE_COST", 1000), help="Annual maintenance cost in USD")
+    parser.add_argument("--full_insurance_cost", type=float, default=os.environ.get("FULL_INSURANCE_COST", 1500), help="Annual full insurance cost in USD")
+    parser.add_argument("--liability_insurance_cost", type=float, default=os.environ.get("LIABILITY_INSURANCE_COST", 800), help="Annual liability insurance cost in USD")
+    parser.add_argument("--years_of_ownership", type=int, default=os.environ.get("YEARS_OF_OWNERSHIP", 5), help="Number of years of ownership for the outright purchase scenario")
+    parser.add_argument("--interest_rate_series", type=str, default=os.environ.get("INTEREST_RATE_SERIES", "CARACBW027SBOG"), help="Interest rate series identifier")
+    parser.add_argument("--fred_api_key", type=str, default=os.environ.get("FRED_API_KEY", None), help="FRED API key to fetch the average interest rate")
 
     args = parser.parse_args()
 
@@ -33,7 +30,7 @@ def main():
     lease_cost = calculate_lease_cost(args.lease_length_months, args.monthly_lease_payment, args.down_payment, args.annual_maintenance_cost, args.full_insurance_cost)
     purchase_cost = calculate_purchase_cost(args.purchase_price, args.annual_maintenance_cost, args.liability_insurance_cost, args.years_of_ownership)
 
-    print("Total cost of getting a loan: ${:.2f}".format(loan_cost))
+    print("\nTotal cost of getting a loan: ${:.2f}".format(loan_cost))
     print("Total cost of leasing a car: ${:.2f}".format(lease_cost))
     print("Total cost of purchasing a car outright: ${:.2f}".format(purchase_cost))
 
